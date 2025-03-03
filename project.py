f2ddfad7521fbe3852797cff223fe751addef97e
athenova <aleksey.sergeyev@yandex.com>
2025-03-03 22:40:20 +0300

    Начало проекта

---
+++ project.py
@@ -0,0 +1,25 @@
+from simple_blogger import Journalist
+from datetime import datetime
+
+class Project(Journalist):
+    def __init__(self, **kwargs):
+        super().__init__(            
+            review_chat_id=-1002374309134,
+            first_post_date=datetime(2025, 3, 4),
+            text_ai_token_name='OPENAI_API_KEY',
+            ai_text_model='chatgpt-4o-latest',
+            text_base_url='https://api.openai.com/v1',
+            **kwargs)
+
+    def _task_converter(self, item):
+        return { 
+                "topic": f"{item['topic']}",
+                "category": f"{item['category']}",
+                "topic_image": f"Нарисуй картинку, вдохновлённую темой '{item['topic']}' из области '{item['category']}'",
+                "topic_prompt": f"Выбери рандомно актуальную проблему по теме '{item['topic']}' из области '{item['category']}', опиши проблему, выбери рандомно метод решения, опиши метод решения, используй смайлики, используй менее {self.topic_word_limit} слов",
+            }
+    
+    def _system_prompt(self, task):
+        return "Ты - проектный менеджер, лидер проекта со 100% харизмой, всегда оптимистично настроенный и с отличным чувством юмора"
+    
+   
---
+++ review.py
@@ -0,0 +1,4 @@
+from project import Project
+
+project = Project()
+project.review(force_image_regen=True, force_text_regen=True)
