5b6f6df86df94a8a7f8b439bdb3da1b8f678ddcd
athenova <aleksey.sergeyev@yandex.com>
2025-03-25 07:54:49 +0300

    Постер в ВК

--- project.py
+++ project.py
@@ -1,14 +1,20 @@
 from simple_blogger import Journalist
 from simple_blogger.generators.OpenAIGenerator import OpenAITextGenerator
+from simple_blogger.senders.TelegramSender import TelegramSender
+from simple_blogger.senders.VkSender import VkSender
 from datetime import datetime
 
 class Project(Journalist):
     def __init__(self, **kwargs):
         super().__init__(            
-            review_chat_id=-1002374309134,
             first_post_date=datetime(2025, 3, 4),
             text_generator=OpenAITextGenerator(),
             send_text_with_image=True,
+            reviewer=TelegramSender(),
+            senders=[
+                TelegramSender(channel_id='@verge_of_breakdown'),
+                VkSender(group_id='229837997')
+            ],
             topic_word_limit=100,
             **kwargs)
 
@@ -22,5 +28,3 @@ class Project(Journalist):
     
     def _system_prompt(self, task):
         return "Ты - проектный менеджер, лидер проекта со 100% харизмой, всегда оптимистично настроенный и с отличным чувством юмора"
-    
-   
